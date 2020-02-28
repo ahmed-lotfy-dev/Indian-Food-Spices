357ef92e7ae3764e5f3db19f44959e85151114bd
alotfy2019 <elshenawy19@gmail.com>
2020-02-28 12:55:31 +0200

    nothing new

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-<script>
-  AOS.init();
-</script>
+document.getElementById("menu-icon").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("menu-icon").classList.toggle("open");
+}
